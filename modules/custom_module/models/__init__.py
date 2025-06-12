87e0018c4139bf1acc5722c18f5e3260b6e2a006
nourfrioui <nourfrioui88@gmail.com>
2025-06-12 12:04:57 +0100

    WIP : debugging

--- modules/custom_module/models/__init__.py
+++ modules/custom_module/models/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from . import res_company_inherit
+# from . import res_company_inherit
 from . import product_template_inherit
 from . import product_category_inherit
 from . import pos_category_inherit
